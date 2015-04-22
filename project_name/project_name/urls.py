8bc2d49223c14a34440356ce2facd02b2d6a6bba
LRPalacios <luisz54@hotmail.com>
2015-04-21 21:32:32 -0600

    add url for serving media files while on debug

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -19,3 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
