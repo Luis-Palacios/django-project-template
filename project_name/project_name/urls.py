5eaebe104c0b44733debb2837932a66c6a047dd8
LRPalacios <luisz54@hotmail.com>
2015-05-06 23:25:01 -0600

    add index template and home page

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.default import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='home' ),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
