d1db2006276589cec253374b15f35cd628969766
LRPalacios <luisz54@hotmail.com>
2015-06-24 20:03:25 -0600

    update middleware classes

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = '{{ project_name }}.urls'
